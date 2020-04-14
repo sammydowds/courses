c2db1972df69206cfe95c9d56833a52b4275505c
Sammy D <sammydowds1993@gmail.com>
2020-04-13 21:15:34 -0500

    First Redux action

--- React_Study/src/components/DishdetailComponent.js
+++ React_Study/src/components/DishdetailComponent.js
@@ -27,8 +27,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   render() {
@@ -101,7 +100,7 @@ class CommentForm extends Component {
   }
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId }) {
   if (comments != null){
     const commentlist = comments.map((comment) => {
       return (
@@ -118,7 +117,7 @@ function RenderComments({comments}) {
         <ListGroup>
           {commentlist}
         </ListGroup>
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment={addComment}/>
       </div>
     )
   }
@@ -160,7 +159,10 @@ const DishDetail = (props) => {
         </div>
         <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments = {props.comments} />
+          <RenderComments comments = {props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}
+          />
         </div>
       </div>
     )
