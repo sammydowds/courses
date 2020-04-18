51d5b4072dcc351989e559236894d64201811612
sammydowds <sammydowds1993@gmail.com>
2020-04-18 15:13:24 -0500

    React animation components

--- React_Study/src/components/DishdetailComponent.js
+++ React_Study/src/components/DishdetailComponent.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -103,26 +105,30 @@ class CommentForm extends Component {
 }
 
 function RenderComments({comments, postComment, dishId }) {
-  if (comments != null){
-    const commentlist = comments.map((comment) => {
-      return (
-        <ListGroupItem>
-            {comment.comment}
-            <br></br>
-             -- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
-        </ListGroupItem>
-      )
-    });
+  if (comments != null) {
     return (
       <div className="col-12 col-md-5 m-1">
-        <h4>Comments</h4>
-        <ListGroup>
-          {commentlist}
-        </ListGroup>
-        <CommentForm dishId={dishId} postComment={postComment}/>
+        <h4> Comments </h4>
+        <ul className="list-unstyled">
+          <Stagger in>
+            {comments.map((comment) => {
+              return(
+                <Fade in>
+                  <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p> -- {comment.author}</p>
+                    <p>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                  </li>
+                </Fade>
+              );
+            })}
+          </Stagger>
+        </ul>
+      <CommentForm dishId={dishId} postComment={postComment}/>
       </div>
-    )
+    );
   }
+  
   else {
     return (
       <div></div>
@@ -133,13 +139,18 @@ function RenderComments({comments, postComment, dishId }) {
 function RenderDish({dish}) {
   return (
     <div className="col-12 col-md-5 m-1">
-      <Card>
-        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+    <FadeTransform in
+      transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+      }}>
+        <Card>
+          <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
     </div>
   );
 }
